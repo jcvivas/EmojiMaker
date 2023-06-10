3e5127539fda4e89ea90a83c66aec0d649fcc471
jcvivas <jcvivas@espol.edu.ec>
2023-06-10 11:32:45 -0500

    primer commmit

---
+++ EmojiMaker/src/main/java/estructuras/emojimaker/PrimaryController.java
@@ -0,0 +1,12 @@
+package estructuras.emojimaker;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
